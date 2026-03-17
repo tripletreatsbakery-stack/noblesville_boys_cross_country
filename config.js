e287972f0a0fd0a544369287f2ff18e79f64d978
tripletreatsbakery-stack <triple.treats.bakery@gmail.com>
2026-03-17 16:48:06 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    SUPABASE_URL: "https://ezohjtwkamhxztjzqbrh.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV6b2hqdHdrYW1oeHp0anpxYnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0OTUwODEsImV4cCI6MjA3MDA3MTA4MX0.3-BQGahVCYWNt96g7qXVTR4AbttCWmXUEZrNPdtqdKo"
+};
